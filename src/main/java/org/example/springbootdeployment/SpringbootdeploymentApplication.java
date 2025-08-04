92c10ae8dbc8ada31dc22ed2628af4ff5c7f4874
nisalsahansith <nisalsahansith@gmail.com>
2025-08-04 10:02:36 +0530

    Springbootdeployment

---
+++ src/main/java/org/example/springbootdeployment/SpringbootdeploymentApplication.java
@@ -0,0 +1,13 @@
+package org.example.springbootdeployment;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringbootdeploymentApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringbootdeploymentApplication.class, args);
+	}
+
+}
